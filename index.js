01690d93d6d84dead4a9734eebcbd597876a7afe
TouqeerH <touqeerhussain.pro@gmail.com>
2024-10-07 21:18:48 +0500

    ADD - heavy task API

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ app.get("/", (req, res) => {
   res.send("A simple HTTP request");
 });
 
+app.get("/heavy-task", (req, res) => {
+  let counter = 0;
+  while (counter < 9000) {
+    counter++;
+  }
+  console.log(`${counter} Iteration request completed`);
+
+  res.end(`${counter} Iteration request completed`);
+});
+
+app.get("/light-task", (req, res) => {
+  res.send("A simple HTTP request");
+});
+
 app.listen(3000, () => console.log("App listening on port 3000"));
